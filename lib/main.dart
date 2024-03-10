e56e09b3190b310a4cfea3eb3713b26c630359b2
ga111o <ga111o@proton.me>
2024-03-10 11:57:23 +0900

    reusable account widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color.fromRGBO(240, 240, 245, 1),
         appBar: const Header(),
--- lib/widgets/account.dart
+++ lib/widgets/account.dart
@@ -35,286 +35,136 @@ class Account extends StatelessWidget {
                 ],
               ),
             ), // Title
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.lightBlueAccent.shade100,
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.star,
-                          size: 30,
-                          color: Colors.white.withOpacity(0.5),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'TossBank Bankbook',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17,
-                                color: Colors.black54),
-                          ),
-                          Text(
-                            '123,123 WON',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 23,
-                                color: Colors.black87),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ), // icon and Text
-                  Container(
-                      height: 50,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'TOSS',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black54),
-                        ),
-                      )), // Text Button
-                ],
-              ),
-            ), // TossBank Bankbook
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.blueAccent.shade200,
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.thunderstorm,
-                          size: 25,
-                          color: Colors.white.withOpacity(0.8),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'TossBank Interest',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17,
-                                color: Colors.black54),
-                          ),
-                          Text(
-                            '123,123 WON',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 23,
-                                color: Colors.black87),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ), // icon and Text
-                  Container(
-                      height: 50,
-                      width: 85,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'GET NOW',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black54),
-                        ),
-                      )), // Text Button
-                ],
-              ),
-            ), // TossBank Interest
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.orangeAccent.withOpacity(0.8),
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.attach_money_rounded,
-                          size: 25,
-                          color: Colors.black12.withOpacity(0.5),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Foreign Currency',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17,
-                                color: Colors.black54),
-                          ),
-                          Text(
-                            '123,123 WON',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 23,
-                                color: Colors.black87),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ), // icon and Text
-                ],
-              ),
-            ), // Foreign Currency
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.pinkAccent.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.money_rounded,
-                          size: 25,
-                          color: Colors.black12.withOpacity(0.5),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Friend Bankbook',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17,
-                                color: Colors.black54),
-                          ),
-                          Text(
-                            '123,123 WON',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 23,
-                                color: Colors.black87),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ), // icon and Text
-                ],
-              ),
-            ), // Friend Bankbook
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                            color: Colors.yellow,
-                            borderRadius: BorderRadius.circular(100)),
-                        child: Icon(
-                          Icons.abc_rounded,
-                          size: 30,
-                          color: Colors.black.withOpacity(0.5),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      const Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Kakaobank',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 17,
-                                color: Colors.black54),
-                          ),
-                          Text(
-                            '123,123 WON',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 23,
-                                color: Colors.black87),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ), // icon and Text
-                  Container(
-                      height: 50,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          'TOSS',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black54),
-                        ),
-                      )), // Text Button
-                ],
-              ),
-            ), // Kakaobank
+            AccountLine(
+              accountName: 'TossBank Bankbook',
+              amount: '123,123',
+              icon: Icons.star,
+              bgColor: Colors.lightBlueAccent.shade100,
+              iconColor: Colors.white.withOpacity(0.5),
+              getButton: false,
+              sideButton: true,
+            ), //TossBank Bankbook
+            AccountLine(
+              accountName: 'TossBank Interest',
+              amount: '123,123',
+              icon: Icons.star,
+              bgColor: Colors.lightBlueAccent.shade100,
+              iconColor: Colors.white.withOpacity(0.5),
+              getButton: true,
+              sideButton: true,
+            ), //
+            AccountLine(
+              accountName: 'Foreign Currency',
+              amount: '123,123',
+              icon: Icons.star,
+              bgColor: Colors.lightBlueAccent.shade100,
+              iconColor: Colors.white.withOpacity(0.5),
+              getButton: false,
+              sideButton: false,
+            ), //
+            AccountLine(
+              accountName: 'wowow',
+              amount: '123,123',
+              icon: Icons.star,
+              bgColor: Colors.lightBlueAccent.shade100,
+              iconColor: Colors.white.withOpacity(0.5),
+              getButton: false,
+              sideButton: false,
+            ), //TossBank Interest
+            AccountLine(
+              accountName: 'Kakaobank',
+              amount: '123,123',
+              icon: Icons.star,
+              bgColor: Colors.lightBlueAccent.shade100,
+              iconColor: Colors.white.withOpacity(0.5),
+              getButton: false,
+              sideButton: true,
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AccountLine extends StatelessWidget {
+  final String accountName, amount;
+  final IconData icon;
+  final Color bgColor, iconColor;
+  final bool sideButton, getButton;
+
+  const AccountLine({
+    super.key,
+    required this.accountName,
+    required this.amount,
+    required this.icon,
+    required this.bgColor,
+    required this.iconColor,
+    required this.sideButton,
+    required this.getButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: bgColor, borderRadius: BorderRadius.circular(100)),
+                child: Icon(
+                  icon,
+                  size: 30,
+                  color: iconColor,
+                ),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    accountName,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                        color: Colors.black54),
+                  ),
+                  Text(
+                    amount,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 23,
+                        color: Colors.black87),
+                  ),
+                ],
+              ),
+            ],
+          ), // icon and Text
+          if (sideButton)
+            Container(
+              height: 50,
+              width: getButton ? 85 : 50,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade100,
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Center(
+                child: Text(
+                  getButton ? 'GET NOW' : 'TOSS',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w700, color: Colors.black54),
+                ),
+              ),
+            ), // Text Button
+        ],
       ),
     );
   }
